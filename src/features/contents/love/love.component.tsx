171b7ecb9ef817c4d176ce16f527d51d089a6262
masrayfa <maselon2030@gmail.com>
2025-10-07 19:48:57 +0700

    release: love content is WIP and still cooking

--- src/features/contents/love/love.component.tsx
+++ src/features/contents/love/love.component.tsx
@@ -1,5 +1,10 @@
 const LoveComponent = () => {
-  return <div>LoveComponent</div>;
+  return (
+    <div className="flex flex-col gap-5 justify-center items-center h-full">
+      <p className="text-8xl">WIP</p>
+      <p className="text-4xl">Still cooking 🔥</p>
+    </div>
+  );
 };
 
 export default LoveComponent;
