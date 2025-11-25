71ad3994a897a634e5635552b16ed5458d9c5ca7
masray <maselon2030@gmail.com>
2025-11-25 21:13:09 +0700

    release: remove unused import

--- src/features/contents/reviews/reviews.component.tsx
+++ src/features/contents/reviews/reviews.component.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react';
 import { Link } from '@tanstack/react-router';
 import ReviewsMDsCollections from './md';
 import { useFilterStore } from '@/stores/filter-store';
