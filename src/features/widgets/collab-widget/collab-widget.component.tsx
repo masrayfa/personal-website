b65655325ef2d2fa1142f3d38959600b5452c7ef
masray <maselon2030@gmail.com>
2025-11-26 19:24:39 +0700

    release: collab comp to call cal.com

--- src/features/widgets/collab-widget/collab-widget.component.tsx
+++ src/features/widgets/collab-widget/collab-widget.component.tsx
@@ -1,19 +1,22 @@
 import { Button } from '@/components/ui/button';
+import { Link } from '@tanstack/react-router';
 import { PiCallBell } from 'react-icons/pi';
 
 const CollabWidget = () => {
   return (
     <div className="p-5">
-      <Button
-        type="button"
-        variant="outline"
-        className="w-full py-3 text-sm font-mono border rounded-none hover:bg-gray-50 cursor-pointer"
-      >
-        <span className="mr-2">
-          <PiCallBell />
-        </span>
-        let's collab
-      </Button>
+      <Link to="https://cal.com/sani.d.uong/30min" target="_blank">
+        <Button
+          type="button"
+          variant="outline"
+          className="w-full py-3 text-sm font-mono border rounded-none hover:bg-gray-50 cursor-pointer"
+        >
+          <span className="mr-2">
+            <PiCallBell />
+          </span>
+          let's collab
+        </Button>
+      </Link>
     </div>
   );
 };
