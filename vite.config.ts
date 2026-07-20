1f8e0521b4b2690ce91307ea5eb0e37e993fc23a
ray <mas.nurimansyah@swiperxapp.com>
2026-07-20 21:55:14 +0700

    fix: match MDX development flag to NODE_ENV

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ const config = defineConfig({
     }),
     tailwindcss(),
     mdx({
+      development: process.env.NODE_ENV !== 'production',
       remarkPlugins: [remarkGfm],
       rehypePlugins: [rehypeHighlight],
     }),
