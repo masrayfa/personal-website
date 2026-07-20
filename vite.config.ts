36ff632e7dad0ccab462c8b1809d23453487d4bb
masray <maselon2030@gmail.com>
2026-07-20 20:19:09 +0700

    fix: configure vite to listen on all network interfaces

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
-import viteTsConfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "@tailwindcss/vite";
-import cloudflare from "./vite-plugins/cloudflare.ts";
-import neon from "./vite-plugins/neon.ts";
-import mdx from "@mdx-js/rollup";
-import remarkGfm from "remark-gfm";
-import rehypeHighlight from "rehype-highlight";
-import { fileURLToPath } from "url";
+import { defineConfig } from 'vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
+import cloudflare from './vite-plugins/cloudflare.ts';
+import neon from './vite-plugins/neon.ts';
+import mdx from '@mdx-js/rollup';
+import remarkGfm from 'remark-gfm';
+import rehypeHighlight from 'rehype-highlight';
+import { fileURLToPath } from 'url';
 
 const config = defineConfig({
   plugins: [
@@ -16,7 +16,7 @@ const config = defineConfig({
     neon,
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
-      projects: ["./tsconfig.json"],
+      projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
     mdx({
@@ -28,11 +28,12 @@ const config = defineConfig({
   ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
     port: 3000,
+    host: '0.0.0.0',
   },
 });
 
